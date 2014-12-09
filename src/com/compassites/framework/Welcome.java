f24edacdc7eb64c0e8b4648596f85cd41cc1584b
shivam01 <mishra.shivam610@gmail.com>
2014-12-09 13:23:15 +0530

    First commit

---
+++ src/com/compassites/framework/Welcome.java
@@ -0,0 +1,12 @@
+package com.compassites.framework;
+
+public class Welcome {
+	public static void main(String args[]){
+		System.out.println("Welcome Screen");
+		WelcomePerformance wpt=	new WelcomePerformance();
+		wpt. DisplayWelcomeMessage();
+		
+		
+	}
+
+}
---
+++ src/com/compassites/framework/WelcomePerformance.java
@@ -0,0 +1,8 @@
+package com.compassites.framework;
+
+class WelcomePerformance {
+	public void DisplayWelcomeMessage(){
+		System.out.println("Welcome");
+		
+	}
+}
